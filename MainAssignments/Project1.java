dc352344e195f97f4ecfc6894d2c794fa9189849
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-10-06 10:09:42 -0400

    Init project 1

---
+++ MainAssignments/Project1.java
@@ -0,0 +1,22 @@
+package MainAssignments;
+/*
+ * Project 1: 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+public class Project1 {
+    public static void main(String[] args) {
+
+    }
+}
