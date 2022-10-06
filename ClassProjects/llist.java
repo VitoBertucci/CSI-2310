173815167d6bb8a57e003bbad1a9fe028285947b
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-10-06 10:04:47 -0400

    Update llist.java

--- ClassProjects/llist.java
+++ ClassProjects/llist.java
@@ -1,14 +1,19 @@
 package ClassProjects;
 import Classes.linkedList;
+import Classes.Node;
 
 public class llist {
+    public static void print(int s) {
+        System.out.println(s);
+    }
+
     //linked list project for lab
     public static void main(String[] args) {
+        
         linkedList list = new linkedList();
-        list.insert(5);
-        list.insertTail(7);
-        list.insertHead(4);
-        list.insertAfter(6, 5);
+        
         list.display();
+        
+        
     }
 }
